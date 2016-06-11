712b9f380f61ee39a44ecb4448d61974ad1d2ac4
mjkamminga@gmail.com <mjkamminga@gmail.com>
2016-06-11 16:26:31 +0200

    Shoppingcart remove route bug fix

--- platform/views/partials/ajaxshoppingcart.php
+++ platform/views/partials/ajaxshoppingcart.php
@@ -1,4 +1,7 @@
 >> section('shoppingcart')
+    <?php
+    $this->service('htmlMessages')->warningClass('callout warning');
+    ?>
    >> partial('partials::shoppingcart.php')
 << section('shoppingcart')
 

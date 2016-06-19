14976dca30bf6d5a1d2ede25abc422842d9c51d9
mjkamminga@gmail.com <mjkamminga@gmail.com>
2016-06-19 20:39:27 +0200

    Cart icon fix

--- platform/views/layout/main.layout.php
+++ platform/views/layout/main.layout.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	>> section('styles')
         <link rel="stylesheet" href="/css/foundation.min.css" />
+        <link rel="stylesheet" href="/css/foundation-icons.css" />
         <link rel="stylesheet" href="/css/main.css" />
     << section('styles')
 
